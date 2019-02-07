0d275fdb063d508ebb085621eec5e1d880bdef6d
Mickael Bolnet <mickael.bolnet@gmail.com>
2019-02-07 11:01:01 +0100

    resize image

--- exo_canvas.py
+++ exo_canvas.py
@@ -5,7 +5,12 @@ from tkinter import Tk, Canvas, PhotoImage
 
 fen = Tk()
 
-canvas = Canvas(fen, width=200, height=200)
+size = (5,5)
+img_size = (160, 160)
+photo = PhotoImage(file="feu.gif")
+canvas = Canvas(fen, width=img_size[0] * size[0], height=img_size[1] * size[1])
 canvas.pack()
+
+canvas.create_image(img_size[0]//2, img_size[1]//2, image=photo)
 
 fen.mainloop()
